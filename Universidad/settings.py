40e7caf9662202b021130b857967b999263a6b1e
Mauricio Frías <66804192+Mauriciofrias22@users.noreply.github.com>
2022-01-26 17:20:36 -0500

    Update settings.py

--- Universidad/settings.py
+++ Universidad/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'es-pe'
+LANGUAGE_CODE = 'es-co'
 
-TIME_ZONE = 'America/Lima'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
